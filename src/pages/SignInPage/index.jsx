934f0677a681aea425509939c5f60f1bf8346e70
Bruna Terribile <brunaterribile@gmail.com>
2023-06-12 14:10:10 -0300

    feat: static sign in layout, route and form

---
+++ src/pages/SignInPage/index.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom'
+import logo from '../../assets/images/icone-sem-fundo.png'
+import { FormBar, SignInContainer } from './styled'
+
+export default function SignIn() {
+    return (
+        <SignInContainer>
+            <div className='logo'>
+                <img src={logo} alt='logo'/>
+                <h1>VizinhApp</h1>
+            </div>
+            <FormBar>
+                <h2>Seja bem-vindo ao melhor aplicativo da vizinhança!</h2>
+                <div className='form'>
+                    <form >
+                        <input
+                            name="email"
+                            type="email"
+                            placeholder="email"
+                            required
+                        />
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                            required
+                        />
+                        <button type='submit'>Entrar</button>
+                    </form>
+                    <Link to="/sign-up">
+                        <p>Ainda não é cadastrado? Crie uma conta!</p>
+                    </Link>
+                </div>
+            </FormBar>
+        </SignInContainer>
+    )
+}
---
+++ src/pages/SignUpPage/index.jsx
@@ -0,0 +1,12 @@
+import { SignUpContainer } from '../SignUpPage/styled'
+
+export default function SignUp() {
+    return (
+        <>
+        <div className='logo'>
+        </div>
+        <SignUpContainer></SignUpContainer>
+        </>
+    )
+}
+
